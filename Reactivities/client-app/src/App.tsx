01c8f5a8c76d660bc4d399ed545bad372c07f231
liviusgrosu <liviusgrosu@gmail.com>
2024-01-22 21:34:31 -0700

    Added axios to packages and pulled from the activities API

--- Reactivities/client-app/src/App.tsx
+++ Reactivities/client-app/src/App.tsx
@@ -1,17 +1,41 @@
+import axios from 'axios';
 import './App.css'
-import { ducks } from './demo'
-import DuckItem from './duckItem'
+import { useState, useEffect } from 'react'
+
+// import { ducks } from './demo'
+// import DuckItem from './duckItem'
 
 function App() {
+  // [] is an empty default value
+  const [activities, setActivites] = useState([]);
+
+  // Triggers when component is rendered
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/acitivities')
+      .then(response => {
+        setActivites(response.data)
+      })
+  }, []);
+
   return (
     <div>
       <h1>Reactivities</h1>
-      {/* For each duck in ducks from demo.ts */}
-      {ducks.map(duck => (
-        // Pass a instantiated duck element into the DuckItem function
-        <DuckItem duck={duck}></DuckItem>
-      ))}
+      <ul>
+        {activities.map((activity: any) => (
+          <li key={activity.id}>
+            {activity.title}
+          </li>
+        ))}
+      </ul>
     </div>
+    // <div>
+    //   
+    //   {/* For each duck in ducks from demo.ts */}
+    //   {ducks.map(duck => (
+    //     // Pass a instantiated duck element into the DuckItem function
+    //     <DuckItem duck={duck}></DuckItem>
+    //   ))}
+    // </div>
   )
 }
 
