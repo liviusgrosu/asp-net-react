987c2945c231b6333b0154fbcf9badf724356ca3
liviusgrosu <liviusgrosu@gmail.com>
2024-01-24 21:46:21 -0700

    Created a post API endpoint for creating activities

--- Reactivities/API/Controllers/ActivitiesController.cs
+++ Reactivities/API/Controllers/ActivitiesController.cs
@@ -21,5 +21,12 @@ namespace API.Controllers
         {
             return await Mediator.Send(new Details.Query { Id = id });
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            await Mediator.Send(new Create.Command { Activity = activity });
+            return Ok();
+        }
     }
 }
---
+++ Reactivities/Application/Activities/Create.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using Domain;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Activities
+{
+    public class Create
+    {
+        public class Command: IRequest
+        {  
+            public Activity Activity { get; set; } 
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task Handle(Command request, CancellationToken cancellationToken)
+            {
+                _context.Activities.Add(request.Activity);
+
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
