b44db5c49eb0c822ceb9a93e8853cde4d170c718
liviusgrosu <liviusgrosu@gmail.com>
2024-01-25 15:03:07 -0700

    Added basic implementation of cancellation token in list controller

--- Reactivities/API/Controllers/ActivitiesController.cs
+++ Reactivities/API/Controllers/ActivitiesController.cs
@@ -12,9 +12,9 @@ namespace API.Controllers
     public class ActivitiesController : BaseApiController
     {
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> GetActivities()
+        public async Task<ActionResult<List<Activity>>> GetActivities(CancellationToken ct)
         {
-            return await Mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query(), ct);
         }
 
         [HttpGet("{id}")]
--- Reactivities/Application/Activities/List.cs
+++ Reactivities/Application/Activities/List.cs
@@ -2,6 +2,7 @@
 using Domain;
 using Persistence;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Application.Activities
 {
@@ -12,14 +13,32 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Query, List<Activity>>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly ILogger<List> _logger;
+
+            public Handler(DataContext context, ILogger<List> logger)
             {
                 // We inject the datacontext as a depedancy 
                 _context = context;
+                _logger = logger;
             }
 
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
+                try
+                {
+                    for (var i = 0;i < 10; i++)
+                    {
+                        cancellationToken.ThrowIfCancellationRequested();
+                        await Task.Delay(1000, cancellationToken);
+                        _logger.LogInformation($"Task {i} has completed");
+                    }
+                }
+                catch (Exception)
+                {
+                    _logger.LogInformation("Task was cancelled");
+                    throw;
+                }
+
                 // This request comes from the API/Controller
                 return await _context.Activities.ToListAsync();
             }
