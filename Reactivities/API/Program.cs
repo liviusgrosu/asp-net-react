442df6dd08b29f87667a0ddf3644b9ad9984a27b
liviusgrosu <liviusgrosu@gmail.com>
2024-01-24 20:09:38 -0700

    Added a mediator to control API requests via query/commands

--- Reactivities/API/Program.cs
+++ Reactivities/API/Program.cs
@@ -1,3 +1,4 @@
+using Application.Activities;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 using System.Linq.Expressions;
@@ -20,6 +21,8 @@ builder.Services.AddCors(opt =>
         policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
     });
 });
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(List.Handler).Assembly));
 
 var app = builder.Build();
 
---
+++ Reactivities/Application/Activities/List.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Domain;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        public class Query : IRequest<List<Activity>> { }
+
+        public class Handler : IRequestHandler<Query, List<Activity>>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                // We inject the datacontext as a depedancy 
+                _context = context;
+            }
+
+            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                // This request comes from the API/Controller
+                return await _context.Activities.ToListAsync();
+            }
+        }
+    }
+}
