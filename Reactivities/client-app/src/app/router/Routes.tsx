b52544f1d73d1c8f3d6ee20f7cbb7e16cc38f9f3
liviusgrosu <liviusgrosu@gmail.com>
2024-02-14 14:01:06 -0700

    Implementing react routers to create routing as well implement the homepage

---
+++ Reactivities/client-app/src/app/router/Routes.tsx
@@ -0,0 +1,19 @@
+import { RouteObject, createBrowserRouter } from "react-router-dom"
+import App from "../layout/App"
+import HomePage from "../../features/activities/home/homePage";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import ActivityForm from "../../features/activities/form/ActivityForm";
+
+export const routes: RouteObject[] = [
+    {
+        path: '/',
+        element: <App/>,
+        children: [
+            {path: '', element: <HomePage/>},
+            {path: 'activities', element: <ActivityDashboard/>},
+            {path: 'createActivity', element: <ActivityForm/>}
+        ]
+    },
+]
+
+export const router = createBrowserRouter(routes);
---
+++ Reactivities/client-app/src/features/activities/home/homePage.tsx
@@ -0,0 +1,9 @@
+import { Container } from "semantic-ui-react";
+
+export default function HomePage() {
+    return (
+        <Container style={{marginTop: '7em'}}>
+            <h1>Home page</h1>
+        </Container>
+    )
+}
