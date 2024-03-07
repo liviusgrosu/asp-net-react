ab24dc4fbab5f22209b18bdab0b119d83a548a3d
liviusgrosu <liviusgrosu@gmail.com>
2024-03-06 20:47:03 -0700

    Added textarea

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -8,6 +8,7 @@ import LoadingComponents from "../../../app/layout/LoadingComponents";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from 'yup';
 import MyTextInput from "../../../app/common/form/MyTextInput";
+import MyTextArea from "../../../app/common/form/MyTextArea";
 
 // activity: selectedActivity is put in as a rename because we have a hook for the current data object that we're editing
 export default observer (function ActivityForm()
@@ -79,7 +80,7 @@ export default observer (function ActivityForm()
                 {({ handleSubmit }) => (
                     <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
                         <MyTextInput name='title' placeholder="Title"/>
-                        <MyTextInput placeholder='Description' name='description'/>
+                        <MyTextArea rows={3} placeholder='Description' name='description'/>
                         <MyTextInput placeholder='Date' name='date'/>
                         <MyTextInput placeholder='City' name='city'/>
                         <MyTextInput placeholder='Venue' name='venue'/>
