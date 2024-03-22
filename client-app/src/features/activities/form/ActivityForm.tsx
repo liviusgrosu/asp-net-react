3f86a641b5700d848dbabf7f34353b2f50dd149b
liviusgrosu <liviusgrosu@gmail.com>
2024-03-21 18:42:48 -0600

    Added custom date picker for formik validation

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react";
-import { Button, FormField, Label, Segment } from "semantic-ui-react";
+import { Button, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { Activity } from "../../../app/models/activity";
 import LoadingComponents from "../../../app/layout/LoadingComponents";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form } from "formik";
 import * as Yup from 'yup';
 import MyTextInput from "../../../app/common/form/MyTextInput";
 import MyTextArea from "../../../app/common/form/MyTextArea";
 import MySelectInput from "../../../app/common/form/MySelectInput";
 import { categoryOptions } from "../../../app/common/options/categoryOptions";
+import MyDateInput from "../../../app/common/form/MyDateInput";
 
 // activity: selectedActivity is put in as a rename because we have a hook for the current data object that we're editing
 export default observer (function ActivityForm()
@@ -53,21 +54,6 @@ export default observer (function ActivityForm()
         }
     }, [id, loadActivity])
 
-    // Call back the edit function in activityForm and add to activity list
-    // function handleSubmit() {
-    //     if (!activity.id) {
-    //         activity.id = uuid();
-    //         createActivity(activity).then(() => navigate(`/activities/${activity.id}`));
-    //     } else {
-    //         updateActivity(activity).then(() => navigate(`/activities/${activity.id}`));
-    //     }
-    // }
-
-    // function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
-    //     const {name, value} = event.target;
-    //     setActivity({...activity, [name]: value})
-    // }
-
     if (loadingInitial)
     {
         return <LoadingComponents content="Loading activities..."/>
@@ -81,12 +67,18 @@ export default observer (function ActivityForm()
                     onSubmit={values => console.log(values)}>
                 {({ handleSubmit }) => (
                     <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
-                        <MyTextInput name='title' placeholder="Title"/>
-                        <MyTextArea rows={3} placeholder='Description' name='description'/>
-                        <MySelectInput options={categoryOptions} placeholder='Category' name='category'/>
-                        <MyTextInput placeholder='Date' name='date'/>
-                        <MyTextInput placeholder='City' name='city'/>
-                        <MyTextInput placeholder='Venue' name='venue'/>
+                        <MyTextInput name='title' placeholder="Title" label="something"/>
+                        <MyTextArea name='description' rows={3} placeholder='Description'/>
+                        <MySelectInput name='category' options={categoryOptions} placeholder='Category'/>
+                        <MyDateInput 
+                            name='date'
+                            placeholderText='Date' 
+                            showTimeSelect
+                            timeCaption="time"
+                            dateFormat='MMMM d, yyyy h:mm aa'
+                        />
+                        <MyTextInput name='city' placeholder='City'/>
+                        <MyTextInput name='venue' placeholder='Venue'/>
                         <Button loading={loading} floated='right' positive type='submit' content='Submit'/>
                         <Button as={Link} to='/activities' floated='right' positive type='button' content='cancel'/>
                     </Form>
