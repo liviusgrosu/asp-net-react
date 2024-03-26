9a9e342e4b2cecce07fb0b567217a203c3caaf4d
liviusgrosu <liviusgrosu@gmail.com>
2024-03-25 19:19:11 -0600

    Added user generation via the seed

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -1,11 +1,27 @@
 ﻿using Domain;
+using Microsoft.AspNetCore.Identity;
 
 namespace Persistence
 {
     public class Seed
     {
-        public static async Task SeedData(DataContext context)
+        public static async Task SeedData(DataContext context, UserManager<AppUser> userManager)
         {
+            if(!userManager.Users.Any())
+            {
+                var users = new List<AppUser>()
+                { 
+                    new AppUser{ DisplayName = "Timmy", UserName = "timmy", Email = "timmy@test.com" },
+                    new AppUser{ DisplayName = "Jimmy", UserName = "jimmy", Email = "jimmy@test.com" },
+                    new AppUser{ DisplayName = "Dimmy", UserName = "dimmy", Email = "dimmy@test.com" }
+                };
+
+                foreach(var user in users)
+                {
+                    await userManager.CreateAsync(user, "Pa$$w0rd");
+                }
+            }
+
             if (context.Activities.Any()) return;
 
             var activities = new List<Activity>
