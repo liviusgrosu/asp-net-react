600161658e7348139df615307026502f19f8b27f
liviusgrosu <liviusgrosu@gmail.com>
2024-03-25 21:38:25 -0600

    Added requirements to register DTO

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,9 +1,16 @@
-﻿namespace API.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
 {
     public class RegisterDto
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password must be complex")]
         public string Password { get; set; }
+        [Required]
         public string DisplayName { get; set; }
         public string Username { get; set; }
     }
