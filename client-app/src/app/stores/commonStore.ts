bc7545c79a2884e40bf9585ce337d766c80c2ec9
liviusgrosu <liviusgrosu@gmail.com>
2024-03-27 21:08:09 -0600

    Save token and redirect to activity page upon receiving  a response back from the server

--- client-app/src/app/stores/commonStore.ts
+++ client-app/src/app/stores/commonStore.ts
@@ -3,11 +3,25 @@ import { ServerError } from "../models/serverError";
 
 export default class CommonStore {
     error: ServerError | null = null;
+    token: string | null | undefined = null
+    appLoaded = false;
+
     constructor () {
         makeAutoObservable(this);
     }
 
     setServerError(error: ServerError) {
         this.error = error;
+    }
+
+    setToken = (token: string | null) => {
+        if (token){
+            localStorage.setItem('jwt', token);
+        }
+        this.token = token;
+    }
+
+    setAppLoaded = () => {
+        this.appLoaded = true; 
     }
 }
