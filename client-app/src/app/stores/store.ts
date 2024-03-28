287572d6f5e843c8b961d5a4d427ab3803369d5e
liviusgrosuMO <lgrosu@metocean.com>
2024-03-27 19:55:36 -0600

    Setup the login store

--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -1,17 +1,20 @@
 import { createContext, useContext } from "react";
 import ActivityStore from "./activityStore";
 import CommonStore from "./commonStore";
+import UserStore from "./userStore";
 
 // Define the store
 interface Store {
     activityStore: ActivityStore
     commonStore: CommonStore
+    userStore: UserStore;
 }
 
 // We instantiate our stores here
 export const store: Store = {
     activityStore: new ActivityStore(),
-    commonStore: new CommonStore()
+    commonStore: new CommonStore(),
+    userStore: new UserStore(),
 }
 
 // We create the react context of the store so its available to use between components
