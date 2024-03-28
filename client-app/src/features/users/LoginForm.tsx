c0c795c231108b2cde0a34ed1719490fcc7fd12f
liviusgrosu <liviusgrosu@gmail.com>
2024-03-27 20:30:19 -0600

    handle error in the login page

--- client-app/src/features/users/LoginForm.tsx
+++ client-app/src/features/users/LoginForm.tsx
@@ -1,6 +1,6 @@
-import { Form, Formik } from "formik";
+import { ErrorMessage, Form, Formik } from "formik";
 import MyTextInput from "../../app/common/form/MyTextInput";
-import { Button } from "semantic-ui-react";
+import { Button, Label } from "semantic-ui-react";
 import { observer } from "mobx-react-lite";
 import { useStore } from "../../app/stores/store";
 
@@ -8,13 +8,17 @@ export default observer (function LoginForm() {
     const {userStore} = useStore();
     return (
         <Formik
-            initialValues={{email: '', password: ''}}
-            onSubmit={values => userStore.login(values)}>
+            initialValues={{email: '', password: '', error: null}}
+            onSubmit={(values, {setErrors}) => userStore.login(values).catch(error => 
+                setErrors({error: 'Invalid email or password'}) )}>
 
-            {({handleSubmit, isSubmitting}) => (
+            {({handleSubmit, isSubmitting, errors}) => (
                 <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
                     <MyTextInput placeholder="Email" name='email'/>
                     <MyTextInput placeholder="Password" name='password' type='password'/>
+                    <ErrorMessage
+                        name='error' render={() => <Label style={{marginBottom: 10 }} basic color='red' content={errors.error}/>}
+                    />
                     <Button loading={isSubmitting} positive content='Login' type="submit" fluids/>
                 </Form>
             )}
