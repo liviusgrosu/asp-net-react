94141c30eb5453ff4fc4b610b3d4bbc3f6065d39
liviusgrosu <liviusgrosu@gmail.com>
2024-04-21 16:04:22 -0600

    Cleaned up UI

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,9 +4,29 @@ import { observer } from 'mobx-react-lite';
 import { Outlet, useLocation } from 'react-router-dom';
 import HomePage from '../../features/home/homePage';
 import { ToastContainer } from 'react-toastify';
+import { useStore } from '../stores/store';
+import { useEffect } from 'react';
+import LoadingComponents from './LoadingComponents';
 
 function App() {
   const location = useLocation();
+
+  const {commonStore, userStore} = useStore();
+
+  useEffect(() => {
+    console.log("Loading app...");
+    if (commonStore.token) {
+      console.log("Found a token in the common store");
+      userStore.getUser().finally(() => commonStore.setAppLoaded());
+    } else {
+      console.log("No token found in the common store");
+      commonStore.setAppLoaded();
+    }
+  }, [commonStore, userStore])
+
+  if (!commonStore.appLoaded) {
+    return <LoadingComponents content='Loading app...'/>
+  }
 
   return (
     <>
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -21,7 +21,7 @@ export default observer(function ActivityDashboard()
 
     if (activityStore.loadingInitial) 
     {
-        return <LoadingComponents content='Loading app'/>
+        return <LoadingComponents content='Loading activities...'/>
     }
 
     return (
