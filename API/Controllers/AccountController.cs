7e7fb7cda5b66995e84446e70f64c2b05a3a9e58
liviusgrosu <liviusgrosu@gmail.com>
2024-04-21 17:31:59 -0600

    Formatted bad request for validationSchema in the FE to parse correctly

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -47,12 +47,14 @@ namespace API.Controllers
         {
             if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.Username))
             {
-                return BadRequest("Username is already taken");
+                ModelState.AddModelError("username", "Username taken");
+                return ValidationProblem(ModelState);
             }
 
             if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
             {
-                return BadRequest("Email is already taken");
+                ModelState.AddModelError("email", "Email taken");
+                return ValidationProblem(ModelState);
             }
 
             var user = new AppUser
