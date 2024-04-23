6aa8ffaa75553f8aaa372662d8751000ea5d757c
liviusgrosu <liviusgrosu@gmail.com>
2024-04-22 19:48:25 -0600

    Create the activity dto which includes a collection of profiles instead of attendees

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -3,30 +3,39 @@ using Domain;
 using Persistence;
 using Microsoft.EntityFrameworkCore;
 using Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Application.Activities;
 
 namespace Application.Details
 {
     public class Details
     {
-        public class Query : IRequest<Result<Activity>> 
+        public class Query : IRequest<Result<ActivityDto>> 
         {
             public Guid Id { get; set; }
         }
 
-        public class Handler : IRequestHandler<Query, Result<Activity>>
+        public class Handler : IRequestHandler<Query, Result<ActivityDto>>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly IMapper _mapper;
+
+            public Handler(DataContext context, IMapper mapper)
             {
                 // We inject the datacontext as a depedancy 
                 _context = context;
+                _mapper = mapper;
             }
 
-            public async Task<Result<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<ActivityDto>> Handle(Query request, CancellationToken cancellationToken)
             {
                 // This request comes from the API/Controller
-                var activity = await _context.Activities.FindAsync(request.Id);
-                return Result<Activity>.Success(activity);
+                var activity = await _context.Activities
+                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
+                    .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+                return Result<ActivityDto>.Success(activity);
             }
         }
     }
--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -5,26 +5,32 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Application.Core;
 using Microsoft.AspNetCore.Http.HttpResults;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 
 namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<Result<List<Activity>>> { }
+        public class Query : IRequest<Result<List<ActivityDto>>> { }
 
-        public class Handler : IRequestHandler<Query, Result<List<Activity>>>
+        public class Handler : IRequestHandler<Query, Result<List<ActivityDto>>>
         {
             private readonly DataContext _context;
             private readonly ILogger<List> _logger;
+            private readonly IMapper _mapper;
 
-            public Handler(DataContext context, ILogger<List> logger)
+            public Handler(DataContext context, 
+                            ILogger<List> logger,
+                            IMapper mapper)
             {
                 // We inject the datacontext as a depedancy 
                 _context = context;
                 _logger = logger;
+                _mapper = mapper;
             }
 
-            public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
                 try
                 {
@@ -36,8 +42,12 @@ namespace Application.Activities
                     throw;
                 }
 
+                var activities = await _context.Activities
+                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
+                    .ToListAsync();
+
                 // This request comes from the API/Controller
-                return Result<List<Activity>>.Success( await _context.Activities.ToListAsync());
+                return Result<List<ActivityDto>>.Success(activities);
             }
         }
     }
