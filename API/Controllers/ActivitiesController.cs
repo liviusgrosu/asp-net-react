1519ee8dbdc47b4a16e87c14804931c2e9c04c8b
liviusgrosu <liviusgrosu@gmail.com>
2024-04-22 20:13:23 -0600

    Implemented endpoint and controller to update the attendance of an activity

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -42,5 +42,11 @@ namespace API.Controllers
         {
             return HandleResult(await Mediator.Send(new Delete.Command { Id = id }));
         }
+
+        [HttpPost("{id}/attend")]
+        public async Task<IActionResult> Attend(Guid id)
+        {
+            return HandleResult(await Mediator.Send(new UpdateAttendance.Command { Id = id }));
+        }
     }
 }
