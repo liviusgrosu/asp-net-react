1804be51937ef8b62038021d8a44ffe578b0a763
liviusgrosu <liviusgrosu@gmail.com>
2024-04-24 20:44:57 -0600

    When creating a form and saving to activity store, convert from activity to activity form

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,6 +1,6 @@
 import { makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
-import { Activity } from "../models/activity";
+import { Activity, ActivityFormValues } from "../models/activity";
 import {format} from 'date-fns';
 import { store } from "./store";
 import { Profile } from "../models/profile";
@@ -89,39 +89,35 @@ export default class ActivityStore {
         this.loadingInitial = state;
     }
 
-    createActivity = async (activity: Activity) => {
-        this.loading = true;
+    createActivity = async (activity: ActivityFormValues) => {
+        const user = store.userStore.user;
+        const attendee = new Profile(user!);
         try {
             await agent.Activities.create(activity);
+            const newActivity = new Activity(activity);
+            newActivity.hostUsername = user!.username;
+            newActivity.attendees = [attendee];
+            this.setActivity(newActivity);
             runInAction(() => {
-                this.activityRegistry.set(activity.id, activity);
-                this.selectedActivity = activity;
-                this.editMode = false;
-                this.loading = false;
+                this.selectedActivity = newActivity;
             })
         } catch (error) {
             console.log(error);
-            runInAction(() => {
-                this.loading = false;
-            })
         }
     }
 
-    updateActivity = async (activity: Activity) => {
-        this.loading = true;
+    updateActivity = async (activity: ActivityFormValues) => {
         try {
             await agent.Activities.update(activity);
             runInAction(() => {
-                this.activityRegistry.set(activity.id, activity);
-                this.selectedActivity = activity;
-                this.editMode = false;
-                this.loading = false;
+                if (activity.id) {
+                    const updatedActivity = {...this.getActivity(activity.id), ...activity}
+                    this.activityRegistry.set(activity.id, updatedActivity as Activity);
+                    this.selectedActivity = updatedActivity as Activity;
+                }
             })
         } catch (error) {
             console.log(error);
-            runInAction(() => {
-                this.loading = false;
-            })
         }
     }
 
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -3,7 +3,7 @@ import { Button, Header, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { Activity } from "../../../app/models/activity";
+import { Activity, ActivityFormValues } from "../../../app/models/activity";
 import LoadingComponents from "../../../app/layout/LoadingComponents";
 import { Formik, Form } from "formik";
 import * as Yup from 'yup';
@@ -18,8 +18,7 @@ import {v4 as uuid} from 'uuid';
 export default observer (function ActivityForm()
 {
     const {activityStore} = useStore();
-    const { loading, 
-            createActivity, 
+    const { createActivity, 
             updateActivity, 
             loadActivity, 
             loadingInitial
@@ -29,15 +28,7 @@ export default observer (function ActivityForm()
     // Hook that navigates back to certain parts of the site (basically <Link/>)
     const navigate = useNavigate();
 
-    const [activity, setActivity] = useState({
-        id: '',
-        title: '',
-        category: '',
-        description: '',
-        date: null,
-        city: '',
-        venue: ''
-    });
+    const [activity, setActivity] = useState<ActivityFormValues>(new ActivityFormValues());
 
     const validationSchema = Yup.object({
         title: Yup.string().required('The activity title is required'),
@@ -49,11 +40,11 @@ export default observer (function ActivityForm()
     })
 
     useEffect(() => {
-        if (id) loadActivity(id).then(activity => setActivity(activity!));
+        if (id) loadActivity(id).then(activity => setActivity(new ActivityFormValues(activity)));
     }, [id, loadActivity]);
 
-    function handleFormSubmit(activity: Activity) {
-        if (activity.id.length === 0) {
+    function handleFormSubmit(activity: ActivityFormValues) {
+        if (!activity.id) {
             const newActivity = {
                 ...activity,
                 id: uuid()
@@ -94,7 +85,7 @@ export default observer (function ActivityForm()
                         <MyTextInput name='venue' placeholder='Venue'/>
                         <Button 
                             disabled= {isSubmitting || !dirty || !isValid}
-                            loading={loading} floated='right' 
+                            loading={isSubmitting} floated='right' 
                             positive type='submit' content='Submit'/>
                         <Button as={Link} to='/activities' floated='right' positive type='button' content='cancel'/>
                     </Form>
