e89e38638d76a794bc3163d3e4798dbb99125fc3
liviusgrosu <liviusgrosu@gmail.com>
2024-04-24 21:03:38 -0600

    Implement popup when hovering over any attendees in activity

--- client-app/src/features/activities/dashboard/ActivityListItemAttendee.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItemAttendee.tsx
@@ -1,7 +1,8 @@
 import { observer } from 'mobx-react-lite';
-import { List, Image } from 'semantic-ui-react';
+import { List, Image, Popup } from 'semantic-ui-react';
 import { Profile } from '../../../app/models/profile';
 import { Link } from 'react-router-dom';
+import ProfileCard from '../../profiles/ProfileCard';
 
 interface Props {
     attendees: Profile[];
@@ -12,9 +13,20 @@ export default observer(function ActivityListItemAttendee({attendees}: Props) {
         <List horizontal>
             {
                 attendees.map(attendee => (
-                    <List.Item key ={attendee.username} as={Link} to={`/profiles/${attendee.username}`}>
-                        <Image size='mini' circular src={attendee.image || '/assets/user.png'}/>       
-                    </List.Item>
+                    <Popup
+                        hoverable
+                        key={attendee.username}
+                        trigger={
+                            <List.Item key ={attendee.username} as={Link} to={`/profiles/${attendee.username}`}>
+                                <Image size='mini' circular src={attendee.image || '/assets/user.png'}/>       
+                            </List.Item>
+                        }
+                    >
+                        <Popup.Content>
+                            <ProfileCard profile={attendee}/>
+                        </Popup.Content>
+                    </Popup>
+
                 ))
             }
         </List>
