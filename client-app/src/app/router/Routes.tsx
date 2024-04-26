e3e34af287ba90e43fce5a842b6209be08c4da83
liviusgrosu <liviusgrosu@gmail.com>
2024-04-25 20:42:37 -0600

    Added a profile page with a simple header

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -7,6 +7,7 @@ import TestErrors from "../../features/errors/TestError";
 import NotFound from "../../features/errors/NotFound";
 import ServerError from "../../features/errors/ServerError";
 import LoginForm from "../../features/users/loginform";
+import ProfilePage from "../../features/profiles/ProfilePage";
 
 export const router = createBrowserRouter([
     {
@@ -18,6 +19,7 @@ export const router = createBrowserRouter([
              // We add these key tags so that react can render them differently
              {path: 'createActivity', element: <ActivityForm key='create'/>},
              {path: 'manage/:id', element: <ActivityForm key='manage'/>},
+             {path: 'profiles/:username', element: <ProfilePage/>},
              {path: 'login', element: <LoginForm/>},
              {path: 'errors', element: <TestErrors/>},
              {path: 'not-found', element: <NotFound/>},
---
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -0,0 +1,12 @@
+import { Grid } from "semantic-ui-react";
+import ProfileHeader from "./ProfileHeader";
+
+export default function ProfilePage () {
+    return (
+        <Grid>
+            <Grid.Column width={16}>
+                <ProfileHeader/>
+            </Grid.Column>   
+        </Grid>
+    )
+}
