017b2fc6e9d9737b4ed291b85c2fd6dc99724baf
liviusgrosu <liviusgrosu@gmail.com>
2024-05-04 14:48:13 -0600

    Added simple implementation of signal for comments for creating and retrieving them

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -38,6 +38,7 @@ namespace API.Extensions
             services.AddScoped<IUserAccessor, UserAccessor>();
             services.AddScoped<IPhotoAccessor, PhotoAccessor>();
             services.Configure<CloudinarySettings>(config.GetSection("Cloudinary"));
+            services.AddSignalR();
 
             return services;
         }
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using API.Extensions;
 using API.Middleware;
+using API.SignalR;
 using Domain;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -40,6 +41,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHub<ChatHub>("/chat");
 
 // Using is used so that it gets picked up by the garbage collector later
 using var scope = app.Services.CreateScope();
