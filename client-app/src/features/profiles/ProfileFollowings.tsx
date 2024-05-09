d59ea0cc5c5ea7be90b82f55c9a04940adca69c0
liviusgrosu <liviusgrosu@gmail.com>
2024-05-09 16:50:08 -0600

    Display each person in the viewed followings tab

---
+++ client-app/src/features/profiles/ProfileFollowings.tsx
@@ -0,0 +1,31 @@
+import { observer } from 'mobx-react-lite';
+import React, { useEffect } from 'react';
+import { Card, Grid, Header, Tab } from 'semantic-ui-react';
+import ProfileCard from './ProfileCard';
+import { useStore } from '../../app/stores/store';
+
+export default observer(function ProfileFollowings() {
+    const {profileStore} = useStore();
+    const {profile, followings, loadFollowings, loadingFollowings} = profileStore;
+
+    useEffect(() => {
+        loadFollowings('following');
+    }, [loadFollowings]);
+
+    return (
+        <Tab.Pane loading={loadingFollowings}>
+            <Grid>
+                <Grid.Column width={16}>
+                    <Header floated='left' icon='user' content={`People following ${profile?.displayName}`}/>
+                </Grid.Column>
+                <Grid.Column width={16}>
+                    <Card.Group itemsPerRow={4}>
+                        {followings.map(profile => (
+                            <ProfileCard key={profile.username} profile={profile}/>
+                        ))}
+                    </Card.Group>
+                </Grid.Column>
+            </Grid>
+        </Tab.Pane>
+    )
+})
