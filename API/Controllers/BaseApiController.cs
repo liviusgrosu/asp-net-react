82d567e26612024cba8eed1a67a2e990d29401ad
liviusgrosu <liviusgrosu@gmail.com>
2024-05-09 20:46:07 -0600

    Added an extension that injects the pagination data into the response header

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,4 +1,5 @@
-﻿using Application.Core;
+﻿using API.Extensions;
+using Application.Core;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -21,6 +22,29 @@ namespace API.Controllers
             }
             if (result.IsSuccess && result.Value != null)
             {
+                return Ok(result.Value);
+            }
+            if (result.IsSuccess && result.Value == null)
+            {
+                return NotFound();
+            }
+            return BadRequest();
+        }
+
+        protected ActionResult HandlePagedResult<T>(Result<PagedList<T>> result)
+        {
+            if (result == null)
+            {
+                return NotFound();
+            }
+            if (result.IsSuccess && result.Value != null)
+            {
+                Response.AddPaginationHeader(
+                    result.Value.CurrentPage,
+                    result.Value.PageSize,
+                    result.Value.TotalCount,
+                    result.Value.TotalPages
+                );
                 return Ok(result.Value);
             }
             if (result.IsSuccess && result.Value == null)
